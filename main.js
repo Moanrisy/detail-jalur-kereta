51ecf964e02d1276d61bf421e34bc3959784622f
moanrisy <moanrisy@gmail.com>
2022-06-03 01:39:19 +0700

    Init

---
+++ main.js
@@ -0,0 +1,10 @@
+const backBtn = document.querySelector('.back-btn')
+const detailStationBtn = document.querySelector('.detail-btn')
+
+backBtn.addEventListener('click', () => {
+    alert('back button pressed')
+})
+
+detailStationBtn.addEventListener('click', () => {
+    alert('detail button pressed')
+})
